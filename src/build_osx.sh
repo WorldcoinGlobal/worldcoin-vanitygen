6efce93bf3324d8ab28021a12d448e5a3610dd9f
Martin Simon <martin@tentwentyfour.lu>
2015-06-17 09:51:58 +0200

    move stuff around

---
+++ src/build_osx.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# bash script for building vanitygen(1) and oclvanitygen(1) on OSX
+# Copyright (c) 2015 Worldcoin Developers
+# Distributed under the MIT/X11 software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+make -f makefile.osx
+for f in ../bin/*; do
+    strip "$f"
+done
